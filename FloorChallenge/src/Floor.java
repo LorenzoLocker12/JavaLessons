32c5b654c419e8dd49cb5f3feefa03e33ee3b579
loloc <lo.locker1202@gmail.com>
2024-05-26 18:40:29 -0300

    feat: FloorChallenge added

---
+++ FloorChallenge/src/Floor.java
@@ -0,0 +1,27 @@
+public class Floor {
+
+    private double width;
+    private double height;
+
+    public double getArea(){
+        return width * height;
+    }
+
+
+
+    public Floor(double width, double height) {
+        if(width < 0){
+            this.width = 0;
+        }
+        else{
+            this.width = width;
+        }
+
+        if(height < 0){
+            this.height= 0;
+        }
+        else{
+            this.height= height;
+        }
+    }
+}
