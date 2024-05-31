35076b69abc0010d2da5eb5a329d0df8abb2fbee
Lorenzo Locker <lo.locker1202@gmail.com>
2024-05-31 20:13:08 -0300

    feat: Added new Cylinder and PoolArea Challenges

---
+++ PoolArea/src/Rectangle.java
@@ -0,0 +1,22 @@
+public class Rectangle {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length > 0 ? length : 0;
+        this.width = width > 0 ? width : 0;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getArea() {
+        return length * width;
+    }
+
+}
